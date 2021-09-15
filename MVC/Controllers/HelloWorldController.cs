51a7c8b53cb457f0051752777ab333b56f332fa4
Admin <Admin@DESKTOP-AAGAB07>
2021-09-15 08:02:41 +0700

    bb

--- MVC/Controllers/HelloWorldController.cs
+++ MVC/Controllers/HelloWorldController.cs
@@ -8,14 +8,22 @@ namespace MVC.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: HelloWorld
-        public ActionResult Index()
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
         {
             return View();
         }
-        public string Welcome()
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return "Helloworld";
+            ViewData["Message"] = "Xin chào" + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
         }
     }
 }
